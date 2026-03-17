212c3593d51571776c876f513d43e475ede804ad
davidrgren <ronngren.david@gmail.com>
2026-03-17 17:06:25 +0100

    created mongodb connection

---
+++ api/database/mongodb/connMongoDB.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+
+const url = process.env.MONGODB_URI as string
+
+export async function connectMongoDB() {
+    try {
+        await mongoose.connect(uri)
+
+        console.log('MongoDB connected')
+    } catch (error) {
+        console.error('MongoDB connection error:', error)
+        process.exit(1)
+    }
+}
