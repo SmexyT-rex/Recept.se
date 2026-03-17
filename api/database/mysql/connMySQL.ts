cf8c46213827485da7806d12ed2fa872ac4126bc
SmexyT-rex <martin.hagelin@hotmail.com>
2026-03-17 16:43:55 +0100

    commit

--- api/database/mysql/connMySQL.ts
+++ api/database/mysql/connMySQL.ts
@@ -4,10 +4,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST || "",
+  user: process.env.MYSQL_USER || "",
+  password: process.env.MYSQL_PASSWORD || "",
+  database: process.env.MYSQL_DATABASE || "",
+  ssl: {
+    ca: process.env.DB_SSL_CA?.replace(/\\n/gm, "\n") || "",
+  },
   waitForConnections: true,
-  connectionLimit: 10,
+  connectionLimit: 5,
 });
