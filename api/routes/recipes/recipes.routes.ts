32b3b2a8d2d0864a5ff8cfcc64c145eedf8e5d91
SmexyT-rex <martin.hagelin@hotmail.com>
2026-03-26 09:04:55 +0100

    merge

--- api/routes/recipes/recipes.routes.ts
+++ api/routes/recipes/recipes.routes.ts
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { Recipe } from "../../models/recipe.js";
-import { findRecipes, postRecipe } from "../../controllers/recipe.contoller.js";
+import {
+  findRecipes,
+  findRecipeById,
+  postRecipe,
+  updateRecipe,
+  deleteRecipe,
+} from "../../controllers/recipe.contoller.js";
 
 const router = Router();
 
 router.get("/", findRecipes);
-
+router.get("/:id", findRecipeById);
 router.post("/", postRecipe);
+router.put("/:id", updateRecipe);
+router.delete("/:id", deleteRecipe);
 
 export default router;
